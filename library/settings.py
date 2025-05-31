3f699f44ced3a76e31455545bdc3ea548d498be4
chinmay2023 <chinmypendke@gmail.com>
2025-05-31 11:26:02 +0530

    attach dataase to this media files

--- library/settings.py
+++ library/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'media_library_db',
+        'USER': 'media_user',
+        'PASSWORD': 'library',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
